af2b76ef7b3e43b219dcd604dbd8392918f82b71
Leo Huang <huangkwokwaileo@gmail.com>
2024-10-08 20:13:56 +0800

    comment

--- frontend-ui-platform-psitip/src/app/page.tsx
+++ frontend-ui-platform-psitip/src/app/page.tsx
@@ -16,3 +16,53 @@ export default function Home() {
     </Box>
   );
 }
+
+/*
+
+interface mapObject{
+  objectId: string;
+  type: string;
+  label: string;
+  content: string;
+  connectedBy: mapObject[];
+  connectedTo: mapObject[];
+}
+
+function generateCode(mapObjects) {
+  const nodes = mapObjects.filter(obj => obj.type !== 'Connection');
+  const edges = mapObjects.filter(obj => obj.type === 'Connection');
+
+  let code = '';
+
+  nodes.forEach(node => {
+    switch (node.type) {
+      case 'Encoder':
+        code += model.add_node(${node.content}, label="${node.label}");\n;
+        break;
+      case 'Decoder':
+        code += model.add_node(${node.content}, label="${node.label}");\n;
+        break;
+      // Add more cases as needed
+    }
+  });
+
+  edges.forEach(edge => {
+    const fromNode = nodes.find(node => node.objectId === edge.connectedBy[0].objectId);
+    const toNode = nodes.find(node => node.objectId === edge.connectedTo[0].objectId);
+    if (fromNode && toNode) {
+      code += model.add_edge(${fromNode.content}, ${toNode.content});\n;
+    }
+  });
+
+  return code;
+}
+
+// Example usage
+const mapObjects = [
+  // Populate with your mapObject instances
+];
+
+const generatedCode = generateCode(mapObjects);
+console.log(generatedCode);
+
+*/
