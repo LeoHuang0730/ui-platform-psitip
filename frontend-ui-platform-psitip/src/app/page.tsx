fe89373012c5f8da8e12cb77ef2df3ff053e5e2b
Leo Huang <huangkwokwaileo@gmail.com>
2024-10-12 17:14:43 +0800

    update react flow

--- frontend-ui-platform-psitip/src/app/page.tsx
+++ frontend-ui-platform-psitip/src/app/page.tsx
@@ -4,24 +4,35 @@ import styles from "./page.module.css";
 import {
   Box,
   Typography,
+  Button,
 } from "@mui/material";
 import { 
   ReactFlow, 
   Background, 
   Controls, 
-  Position
+  Position,
+  ControlButton,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import { useState } from "react";
+import { 
+  useState,
+  useEffect,
+  useCallback,
+} from "react";
+import { FaRegPlusSquare } from "react-icons/fa";
 
 interface mapNode {
-  objectId: string;
-  type: string;
-  label: string;
-  content: string;
-  rate?: string;
-  connectedBy: string[];
-  connectedTo: string[];
+  id: string;
+  position: {
+    x: number;
+    y: number;
+  }
+  data: {
+    [key: string]: any;
+  }
 }
 
 interface mapEdge {
@@ -34,87 +45,139 @@ export default function Home() {
 
   const [mapNodes, setMapNodes] = useState<mapNode[]>([
     {
-      objectId: '1',
-      type: 'message',
-      label: 'Initial Message',
-      content: 'Initial Message',
-      rate: '999',
-      connectedBy: [],
-      connectedTo: ['2'],
+      id: '1',
+      position: {
+        x: 100,
+        y: 100,
+      },
+      data: {
+        type: 'message',
+        label: 'Initial Message',
+        content: 'Initial Message',
+        rate: '999',
+        connectedBy: [],
+        connectedTo: ['2'],
+      },
     },
     {
-      objectId: '2',
-      type: 'encoder',
-      label: 'Encoder 1',
-      content: 'Encoder 1',
-      connectedBy: ['1'],
-      connectedTo: ['3'],
+      id: '2',
+      position: {
+        x: 200,
+        y: 200,
+      },
+      data: {
+        type: 'encoder',
+        label: 'Encoder 1',
+        content: 'Encoder 1',
+        rate: '',
+        connectedBy: ['1'],
+        connectedTo: ['3'],
+      },
     },
     {
-      objectId: '3',
-      type: 'message',
-      label: 'Encoded Message',
-      content: 'Encoded Message',
-      connectedBy: ['2'],
-      connectedTo: ['4'],
+      id: '3',
+      position: {
+        x: 300,
+        y: 300,
+      },
+      data: {
+        type: 'message',
+        label: 'Encoded Message',
+        content: 'Encoded Message',
+        rate: '',
+        connectedBy: ['2'],
+        connectedTo: ['4'],
+      },
     },
     {
-      objectId: '4',
-      type: 'decoder',
-      label: 'Decoder 1',
-      content: 'Decoder 1',
-      connectedBy: ['3'],
-      connectedTo: ['5'],
+      id: '4',
+      position: {
+        x: 400,
+        y: 400,
+      },
+      data: {
+        type: 'decoder',
+        label: 'Decoder 1',
+        content: 'Decoder 1',
+        rate: '',
+        connectedBy: ['3'],
+        connectedTo: ['5'],
+      },
     },
     {
-      objectId: '5',
-      type: 'message',
-      label: 'Decoded Message',
-      content: 'Decoded Message',
-      connectedBy: ['4'],
-      connectedTo: [],
+      id: '5',
+      position: {
+        x: 500,
+        y: 500,
+      },
+      data: {
+        type: 'message',
+        label: 'Decoded Message',
+        content: 'Decoded Message',
+        rate: '', 
+        connectedBy: ['4'],
+        connectedTo: [],
+      },
     }
   ]);
 
   const [mapEdges, setMapEdges] = useState<mapEdge[]>([]);
 
-  const nodeTranslator = (nodes: mapNode[]) => {
-    const reactFlowNodes: any[] = [];
-    for (let i = 0; i < nodes.length; i++) {
-      if (nodes[i].connectedBy.length > 0) {
-        for (let j = 0; j < nodes[i].connectedBy.length; j++) {
-          mapEdges.push({
-            id: nodes[i].connectedBy[j] + '-' + nodes[i].objectId,
-            source: nodes[i].connectedBy[j],
-            target: nodes[i].objectId,
-          })
+  const onNodesChange = useCallback(
+    (changes:any) => setMapNodes((nds) => applyNodeChanges(changes, nds)),
+    [],
+  );
+
+  const onEdgesChange = useCallback(
+    (changes:any) => setMapEdges((eds) => applyEdgeChanges(changes, eds)),
+    [],
+  );
+
+  const onConnect = useCallback(
+    (params:any) => setMapEdges((eds) => addEdge(params, eds)),
+    [],
+  );
+
+  const addNode = useCallback(() => {
+    // yPos.current += 50;
+    setMapNodes((mapNodes) => {
+      console.log(mapNodes);
+      return [
+        ...mapNodes,
+        {
+          id: '6',
+          position: { x: 0, y: 0 },
+          data: { label: "test" }
         }
-      }
-      console.log(mapEdges);
-      reactFlowNodes.push({
-        id: nodes[i].objectId,
-        position: {
-          x: 100*(i+1),
-          y: 100*(i+1),
-        },
-        data: {
-          label: nodes[i].label,
-        },
-      })
-    }
-    console.log(reactFlowNodes);
-    return reactFlowNodes;
-  }
+      ];
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(mapNodes);
+  }, [mapNodes]);
 
   return (
     <Box>
       <Box width={'100vw'} height={'100vh'}>
         <ReactFlow
-          nodes={nodeTranslator(mapNodes)}
+          nodes={mapNodes}
+          onNodesChange={onNodesChange}
           edges={mapEdges}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          fitView
         >
           <Background />
-          {/* <Controls /> */}
+          <Controls>
+            <ControlButton
+              onClick={() => {
+                addNode();
+              }}
+            >
+              <FaRegPlusSquare />
+            </ControlButton>
+          </Controls>
         </ReactFlow>
       </Box>
     </Box>
